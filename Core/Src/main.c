0838a095b6773deff86982c1f3f6013879ceb220
russeree <git@qrsnap.io>
2024-01-20 11:05:22 -0800

    DFU scatter file reversion

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -125,7 +125,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  //MX_SPI2_Init();
+  MX_SPI2_Init();
   MX_SPI4_Init();
   MX_I2C1_Init();
   MX_TIM7_Init();
